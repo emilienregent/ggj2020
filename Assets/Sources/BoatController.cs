808abeb7760030ea725453b648e220800cf520c4
JejeG <garciajeromejeje@hotmail.fr>
2020-02-01 10:44:50 +0100

    Boat controller

---
+++ Assets/Sources/BoatController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BoatController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5f;
+
+    [SerializeField]
+    private float _rotationSpeed = 3f;
+
+    [SerializeField]
+    private float _maxRotationAngle = 45f;
+
+    [SerializeField]
+    private float _maxTop = 4f;
+    [SerializeField]
+    private float _maxBottom = -4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var gamepad = Gamepad.current;
+        if(gamepad == null)
+            return; // No gamepad connected.
+
+        Vector2 move = gamepad.leftStick.ReadValue();
+        Vector3 moveVector = new Vector3(move.x, move.y, 0);
+
+        if(moveVector != Vector3.zero)
+        {
+            Quaternion rotationMin = Quaternion.Euler(new Vector3(0f, 0f, _maxRotationAngle * -1));
+            Quaternion rotationMax = Quaternion.Euler(new Vector3(0f, 0f, _maxRotationAngle));
+            Quaternion rotation = transform.rotation;
+
+            if(move.x < 0 && rotation.z < rotationMax.z)
+            {
+                rotation.z += Quaternion.Euler(new Vector3(0f, 0f, _rotationSpeed * Time.deltaTime)).z;
+            }
+
+            if(move.x > 0 && rotation.z > rotationMin.z)
+            {
+                rotation.z -= Quaternion.Euler(new Vector3(0f, 0f, _rotationSpeed * Time.deltaTime)).z;
+            }
+            transform.rotation = rotation;
+
+        }
+
+        float transformAngleZ = transform.rotation.eulerAngles.z - 180;
+
+        Vector3 newPosition = transform.position + (Vector3.down * Mathf.Sin(transformAngleZ * Mathf.Deg2Rad) * _speed * Time.deltaTime);
+        newPosition.y = Mathf.Clamp(newPosition.y, _maxBottom, _maxTop);
+
+        transform.position = newPosition;
+    }
+}
