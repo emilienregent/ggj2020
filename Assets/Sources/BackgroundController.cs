59c37d5d26e2b09bb19958bf23d7eb70f2fc1a2e
Emilien REGENT <emilien.regent@gmail.com>
2020-02-01 01:48:08 +0100

    Background system with tile and movement

--- Assets/Sources/BackgroundController.cs
+++ Assets/Sources/BackgroundController.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private void Start()
+    public const float TILE_WORLD_SIZE = 16f;
+
+    private Vector2 _position = Vector2.zero;
+    private Vector2 _leftCorner = Vector2.zero;
+
+    private BackgroundManager _manager = null;
+
+    public void Initialize(BackgroundManager manager)
     {
-        
+        _manager = manager;
+        _position = transform.position;
+    }
+
+    private void Update()
+    {
+        if (IsOutOfLeftBoundary() == true)
+        {
+            _position = _manager.SwapToEnd(this);
+        }
+
+        _position.x -= _manager.speed * Time.deltaTime;
+
+        transform.position = _position;
+    }
+
+    // Check if background has reached the left side of the screen
+    public bool IsOutOfLeftBoundary()
+    {
+        return transform.position.x + TILE_WORLD_SIZE * 2f < _leftCorner.x;
     }
 }
---
+++ Assets/Sources/BackgroundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+	public float speed = 0f;
+    public List<BackgroundController> backgrounds = new List<BackgroundController>();
+
+    // Initialize backgrounds to have proper position
+    private void Start()
+    {
+        for (int i = 0; i < backgrounds.Count; ++i)
+        {
+            backgrounds[i].transform.position = new Vector2((i - 1) * BackgroundController.TILE_WORLD_SIZE, 0f);
+
+            backgrounds[i].Initialize(this);
+        }
+    }
+
+    // Move a background to last position and update the list
+    public Vector2 SwapToEnd(BackgroundController background)
+    {
+        Vector2 position = background.transform.position;
+        BackgroundController lastBackground = backgrounds[backgrounds.Count - 1];
+
+        backgrounds.Remove(background);
+        backgrounds.Add(background);
+
+        position.x = lastBackground.transform.position.x + BackgroundController.TILE_WORLD_SIZE;
+
+        return position;
+    }
+}
