7d7e013850ad60b8151e0c08683052ec7bea4aeb
Simon-Ks <simon.kardous@gmail.com>
2020-02-01 08:38:36 +0100

    wip jobs and interactions

---
+++ Assets/Sources/PositionModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionModel : MonoBehaviour
+{
+
+    public Jobs job;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
